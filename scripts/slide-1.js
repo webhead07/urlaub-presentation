a093f3073d90f1a5557b58bac5446735079a68c9
Asfand <yar.asfand.off@gmail.com>
2026-06-22 14:56:54 +0200

    add slide 2

--- scripts/slide-1.js
+++ scripts/slide-1.js
@@ -1,4 +1,31 @@
-window.addEventListener("click", () => {
-    window.location.href = "/pages/slide-2.html"
+window.addEventListener("keydown", (event) => {
+    if (event.key === " ") {
+        window.location.href = "/pages/slide-2.html"
+    }
 }
 )
+
+const points = [
+    "The pretty/ugly side (we see)",
+    "Provides information in some way e.g visual, audio",
+    "Provides a way for user to interact with the website",
+    "Takes information from user for specific actions",
+    "All in all, its the face of the website that impresses user with animations and cool stuff to make it more appealing!"
+]
+const ul = document.querySelector("ul")
+let count = 0
+
+function addPoints() {
+    const li = document.createElement("li")
+    if (count <= points.length - 1) {
+        li.textContent = points[count]
+        ul.appendChild(li)
+        count += 1
+    }
+
+    if (count >= points.length) {
+        window.removeEventListener("click", addPoints)
+    }
+}
+
+window.addEventListener("click", addPoints)
